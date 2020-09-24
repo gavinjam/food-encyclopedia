e4314a34a4681d75eba605102fdcafaed5b04a21
Gavin Jampani <gavin.jampani@yahoo.com>
2020-09-24 16:15:02 -0700

    FILE-TRANSFER-BUGS: Fixed bug 1 about entrypoint for running the docker container.

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -15,7 +15,7 @@ docker build -f Dockerfile -t food_image .
 # Without -d, enter inside docker container while running (after leaving the 
 # container, it is not running anymore)
 # docker run -it --rm -d --name test-app --entrypoint=/bin/bash 84ed84f93d6c
-docker run -it --rm -d --name test-app --entrypoint=/bin/bash food_image
+docker run -it --rm -d --name test-app --entrypoint=bash food_image
 # get id and pass into the script
 # then run docker cp $docker_id:/Users/gavinjampani/go/src/src .
 
